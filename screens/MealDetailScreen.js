e28af9a2bf3b7d3c0e63cadda5af457c609ece6d
Joshua Radula <radulajoshua@gmail.com>
2025-03-12 14:24:54 +0300

    Added a details screen and titles

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,36 @@
+import { useLayoutEffect } from "react";
+import { Image, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+
+export default function MealDetailScreen({ route, navigation }) {
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: selectedMeal.title,
+    });
+  }, [mealId, navigation]);
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
+}
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,38 +1,50 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
 
-export default function MealsOverviewScreen({ route }) {
-    const catId = route.params.categoryId;
+export default function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
 
-    const displayedMeals = MEALS.filter((mealItem) => {
-        return mealItem.categoryIds.indexOf(catId) >= 0;
-    })
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
 
-    function renderMealItem(itemData) {
-        const item = itemData.item
-        const mealItemProps = {
-            title: item.title,
-            imageUrl: item.imageUrl,
-            duration: item.duration,
-            affordability: item.affordability,
-            complexity: item.complexity,
-        }
-        return (
-            <MealItem {...mealItemProps} />
-        )
-    }
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
 
-    return (
-        <View style={styles.container}>
-            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
-        </View>
-    )
+    navigation.setOptions({
+      title: categoryTitle
+    });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      affordability: item.affordability,
+      complexity: item.complexity,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    },
-})
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
